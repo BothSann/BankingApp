808baa079777872f7a75230e4f7479a1e00f17d5
BothSann <boththann76@gmail.com>
2025-02-03 16:01:46 +0700

    Refactor project to include ICustomer interface
    Modified BSBank.Entities.csproj to remove Class1.cs and include Contracts\ICustomer.cs. Removed Class1.cs file. Added ICustomer.cs defining ICustomer interface with properties for CustomerID, CustomerCode, CustomerName, Address, Landmark, City, Country, and Phone.

---
+++ BSBank.Entities/Contracts/ICustomer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace BSBank.Entities
+{
+    public interface ICustomer
+    {
+        Guid CustomerID { get; set; }
+        long CustomerCode { get; set; } 
+        string CustomerName { get; set; }
+        string Address { get; set; }    
+        string Landmark { get; set; }
+        string City { get; set; }   
+        string Country { get; set; }    
+        string Phone { get; set; }
+
+    }
+}
+    
