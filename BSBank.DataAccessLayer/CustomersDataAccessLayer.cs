20a98e42fb23251c13bb3c283e9da1e2f04dde11
BothSann <boththann76@gmail.com>
2025-02-06 14:32:03 +0700

    Add exception handling to CustomersDataAccessLayer methods
    Introduced `try-catch` blocks to handle `CustomerException` and
    general `Exception` types in the `CustomersDataAccessLayer` class.
    - `GetCustomers`: Added `try-catch` around customer list creation.
    - `GetCustomersByCondition`: Wrapped filtering and copying in `try-catch`.
    - `AddCustomer`: Added `try-catch` for `Guid` generation and addition.
    - `UpdateCustomer`: Included `try-catch` for finding and updating.
    - `DeleteCustomer`: Added `try-catch` for customer removal by `customerID`.
    Each method now throws exceptions if they occur, with `UpdateCustomer`
    and `DeleteCustomer` returning `false` if the customer is not found.

--- BSBank.DataAccessLayer/CustomersDataAccessLayer.cs
+++ BSBank.DataAccessLayer/CustomersDataAccessLayer.cs
@@ -40,77 +40,133 @@ namespace BSBank.DataAccessLayer
         /// <returns>Customers List</returns>
         public List<Customer> GetCustomers()
         {
-            // Create new customers list
-            List<Customer> customersList = new List<Customer>();
+            try
+            {
+                // Create new customers list
+                List<Customer> customersList = new List<Customer>();
 
-            // Copy all customers from source collection into newCustomers list
-            Customers.ForEach(item => customersList.Add(item.Clone() as Customer));
-            return customersList;
+                // Copy all customers from source collection into newCustomers list
+                Customers.ForEach(item => customersList.Add(item.Clone() as Customer));
+                return customersList;
+            }
+            catch (CustomerException)
+            {
+                throw;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            
         }
 
         public List<Customer> GetCustomersByCondition(Predicate<Customer> condition)
         {
-            // Create new customers list
-            List<Customer> customersList = new List<Customer>();
+            try
+            {
+                // Create new customers list
+                List<Customer> customersList = new List<Customer>();
 
-            // Filer the collection 
-            List<Customer> fitleredCustomers = Customers.FindAll(condition);
+                // Filer the collection 
+                List<Customer> fitleredCustomers = Customers.FindAll(condition);
 
-            // Copy all customers from source collection into newCustomers list
-            fitleredCustomers.ForEach(item => customersList.Add(item.Clone() as Customer));
+                // Copy all customers from source collection into newCustomers list
+                fitleredCustomers.ForEach(item => customersList.Add(item.Clone() as Customer));
 
-            return customersList;
+                return customersList;
+            }
+            catch (CustomerException)
+            {
+                throw;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
         }
 
         public Guid AddCustomer(Customer customer)
         {
-            // Generate new Guid
-            customer.CustomerID = Guid.NewGuid();
+            try
+            {
+                // Generate new Guid
+                customer.CustomerID = Guid.NewGuid();
 
-            // Add customer to the collection
-            Customers.Add(customer);
+                // Add customer to the collection
+                Customers.Add(customer);
 
-            return customer.CustomerID;
+                return customer.CustomerID;
+            } 
+            catch (CustomerException)
+            {
+                throw;
+            }
+            catch (Exception) 
+            {
+                throw;
+            }
         }
 
         public bool UpdateCustomer(Customer customer)
         {
-            // Find the customer in the collection
-            Customer existingCustomer = Customers.Find(item => item.CustomerID.Equals(customer.CustomerID));
-
-            // Update all details of custome
-            if (existingCustomer != null)
+            try
             {
-                existingCustomer.CustomerCode = customer.CustomerCode;
-                existingCustomer.CustomerName = customer.CustomerName;
-                existingCustomer.Address = customer.Address;
-                existingCustomer.Landmark = customer.Landmark;
-                existingCustomer.City = customer.City;
-                existingCustomer.Country = customer.Country;
-                existingCustomer.Mobile = customer.Mobile;
+                // Find the customer in the collection
+                Customer existingCustomer = Customers.Find(item => item.CustomerID.Equals(customer.CustomerID));
 
-                return true; // Customer updated successfully
+                // Update all details of custome
+                if (existingCustomer != null)
+                {
+                    existingCustomer.CustomerCode = customer.CustomerCode;
+                    existingCustomer.CustomerName = customer.CustomerName;
+                    existingCustomer.Address = customer.Address;
+                    existingCustomer.Landmark = customer.Landmark;
+                    existingCustomer.City = customer.City;
+                    existingCustomer.Country = customer.Country;
+                    existingCustomer.Mobile = customer.Mobile;
+
+                    return true; // Customer updated successfully
+                }
+                else
+                {
+                    return false; // Customer not found
+                }
             }
-            else
+            catch (CustomerException)
             {
-                return false; // Customer not found
+                throw;
             }
-            #endregion
+            catch (Exception)
+            {
+                throw;
+            }
         }
 
         public bool DeleteCustomer(Guid customerID)
         {
-            // Delete customer by customerID
-            if (Customers.RemoveAll(item => item.CustomerID.Equals(customerID)) > 0)
+            try
             {
-                return true; // One or more customers deleted
-            }
-            else
-            {
-                return false; // No customer is deleted
+                // Delete customer by customerID
+                if (Customers.RemoveAll(item => item.CustomerID.Equals(customerID)) > 0)
+                {
+                    return true; // One or more customers deleted
+                }
+                else
+                {
+                    return false; // No customer is deleted
 
+                }
             }
+            catch (CustomerException)
+            {
+                throw;
+            }
+            catch
+            {
+                throw;
+            }  
         }
-
+        #endregion
     }
 }
