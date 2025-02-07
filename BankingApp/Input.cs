d8091e1c56b2acce2b30c5749b7e91aeef50f578
BothSann <boththann76@gmail.com>
2025-02-07 15:34:19 +0700

    Add Input class with GetUserChoice method to BankingApp

---
+++ BankingApp/Input.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace BankingApp
+{
+    public static class Input
+    {
+        public static int GetUserChoice()
+        {
+            int mainMenuChoice;
+            Console.Write("Enter choice (0-5): ");
+            mainMenuChoice = int.Parse(Console.ReadLine());
+            Console.WriteLine("");
+
+            return mainMenuChoice;
+        }
+    }
+}
