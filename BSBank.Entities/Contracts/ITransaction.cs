a8449d83f400c88b7d74cfcaba0eb724e45e27f0
BothSann <boththann76@gmail.com>
2025-02-15 12:41:34 +0700

    Add ITransaction interface to define transaction properties

---
+++ BSBank.Entities/Contracts/ITransaction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BSBank.Entities.Contracts
+{
+    public interface ITransaction
+    {
+        #region Properties
+        Guid TransactionID { get; set; }
+        decimal Amount { get; set; }
+        Guid DestinationAccountID { get; set; }
+        Guid SourceAccountID { get; set; }
+        DateTime TransactionDateTime { get; set; }
+        #endregion
+    }
+}   
